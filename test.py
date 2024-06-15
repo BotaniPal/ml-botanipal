293a9984878945c75dc2639115d206debc95d4f6
Sandria Amelia Putri <21083010005@student.upnjatim.ac.id>
2024-06-15 22:11:33 +0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://deploy-plant-type-detection-5fhrsocmfa-et.a.run.app"
-data = {
-    "file_url": "https://storage.googleapis.com/backend-nodejs-tes.appspot.com/predictions/iepEjK1YtodBfCD2ZPYC8MDaWUR2_1718348727413.jpg"
-}
-response = requests.post(url, json=data)
-
-print(response.json())
